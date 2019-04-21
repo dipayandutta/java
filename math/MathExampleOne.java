11c50acca9f2ebffa25ccaba48458d814c0548bb
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-04-21 23:22:44 +0530

    math example

---
+++ math/MathExampleOne.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class MathExampleOne{
+  public static void main(String args[]){
+    double x = 20;
+    double y = 30;
+
+    // Max of the two numbers 
+    System.out.println("Max of the two numbers is "+Math.max(x,y));
+
+    // Return the root of y
+    System.out.println("Square root of y variable is "+Math.sqrt(y));
+
+    // Power 
+    System.out.println("x^y is "+Math.pow(x,y));
+
+    // Log
+    System.out.println("log(x) "+Math.log(x));
+    System.out.println("log(y) "+Math.log(y));
+  
+    // exp
+    System.out.println("exponential "+Math.exp(x));
+    System.out.println("exponential "+Math.exp(x));
+  }
+}
